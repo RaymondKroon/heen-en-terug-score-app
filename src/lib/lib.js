210ee8bc3a15c50a84cea9c13142ac691091d852
Raymond <raymond@k3n.nl>
2023-08-20 22:22:12 +0200

    wip smaller protobuf + tests

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -25,3 +25,54 @@ export const TRUMPS_SHORT = {
     3: 'R',
     4: '-',
 }
+
+export function intNArrayToInt32(intN, array) {
+    let wrapper = 0;
+    for (let i = 0; i < array.length; i++) {
+        wrapper |= (array[i] << (i * intN));
+    }
+    return {wrapper, length: array.length, intN};
+}
+
+export function int32ToIntNArray(intN, int32, n) {
+    const array = [];
+    for (let i = 0; i < n; i++) {
+        array.push((int32 >> (i * intN)) & ((1 << intN) - 1));
+    }
+    return array;
+}
+
+export function calculateBitsForNumber(number) {
+    number = BigInt(number);
+    let highestBit = 0;
+    while (number > 0n) {
+        number >>= 1n;
+        highestBit++;
+    }
+    return highestBit;
+}
+
+const rounds = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const bits = rounds.map(round => calculateBitsForNumber(round));
+
+export function playerBidsToInt64(bids) {
+    let result = 0n;
+    let bitshift = 0n;
+    bids.map((bid, idx) => {
+        let bigIntBid = BigInt(bid);
+        result |= (bigIntBid << bitshift);
+        bitshift += BigInt(bits[idx]);
+    });
+
+    return result;
+}
+
+export function int64ToPlayerBids(int64) {
+const bids = [];
+    let bitshift = 0n;
+    rounds.forEach((round, index) => {
+        bids.push(Number((int64 >> bitshift) & ((1n << BigInt(bits[index])) - 1n)));
+        bitshift += BigInt(bits[index]);
+    });
+    return bids;
+}
---
+++ src/lib/lib.test.js
@@ -0,0 +1,74 @@
+import { expect, test } from 'vitest'
+import {
+    intNArrayToInt32,
+    int32ToIntNArray,
+    calculateBitsForNumber,
+    playerBidsToInt64,
+    int64ToPlayerBids
+} from './lib.js'
+
+test('save 5 int4', () => {
+    let result = intNArrayToInt32(4, [1,2,2,2,2]);
+    expect(result.length).toBe(5);
+    expect(result.wrapper).toBe(0b100010001000100001);
+
+    let original = int32ToIntNArray(4, result.wrapper, result.length);
+    expect(original).toEqual([1,2,2,2,2]);
+})
+
+test('save 3 int4', () => {
+    let result = intNArrayToInt32(4, [3, 4, 4]);
+    expect(result.length).toBe(3);
+
+    let original = int32ToIntNArray(4, result.wrapper, result.length);
+    expect(original).toEqual([3, 4, 4]);
+})
+
+test('save 5 int1', () => {
+    let result = intNArrayToInt32(1, [1,1,0,1,0]);
+    expect(result.length).toBe(5);
+    expect(result.wrapper).toBe(0b1011);
+
+    let original = int32ToIntNArray(1, result.wrapper, result.length);
+    expect(original).toEqual([1,1,0,1,0]);
+})
+
+test('save 4 int2', () => {
+    let result = intNArrayToInt32(2, [1,1,0,1,0]);
+    expect(result.length).toBe(5);
+    expect(result.wrapper).toBe(0b1000101);
+
+    let original = int32ToIntNArray(2, result.wrapper, result.length);
+    expect(original).toEqual([1,1,0,1,0]);
+})
+
+test('print bits for rounds', () => {
+    let nRounds = 0;
+    let nBits = 0;
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach(round => {
+        console.log(`${round} cards: ${calculateBitsForNumber(round)} bits`);
+        nRounds++;
+        nBits += calculateBitsForNumber(round);
+    })
+    console.log(`Total: ${nRounds} rounds, ${nBits} bits`);
+})
+
+test('player bids to int64', () => {
+    let bids = [1, 1, 2, 2, 0, 0, 1, 1, 0, 0, 0, 0, 2, 0, 0, 1, 1, 3, 3];
+    let result = playerBidsToInt64(bids);
+    let bits = calculateBitsForNumber(result);
+    expect(bits).toBe(55); // 57 - 2
+    let original = int64ToPlayerBids(result);
+    expect(original).toEqual(bids);
+})
+
+test('player bids to int64, incomplete round', () => {
+    let bids = [1, 1, 2, 2, 0, 0, 1, 1, 0, 0];
+    let result = playerBidsToInt64(bids);
+    let bits = calculateBitsForNumber(result);
+    expect(bits).toBe(25);
+    let original = int64ToPlayerBids(result);
+    expect(original.length).toBe(19);
+    expect(original.slice(0, 10)).toEqual(bids);
+    expect(original.slice(10)).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+})
